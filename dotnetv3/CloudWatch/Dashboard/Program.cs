963fea5ea7addd07065fad0830c5863742526bfd
Holly DuCoing <71049594+HDuCoing@users.noreply.github.com>
2023-07-03 15:13:46 +1200

    added main()

--- dotnetv3/CloudWatch/Dashboard/Program.cs
+++ dotnetv3/CloudWatch/Dashboard/Program.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Console;
 using Microsoft.Extensions.Logging.Debug;
 using CloudWatchActions;
+using CloudWatchScenario;
 
 namespace Program;
 public class Program
@@ -23,7 +24,40 @@ public class Program
 
     static async Task Main(string[] args)
     {
+        // Set up dependency injection for the Amazon service.
+        using var host = Host.CreateDefaultBuilder(args)
+            .ConfigureLogging(logging =>
+                logging.AddFilter("System", LogLevel.Debug)
+                    .AddFilter<DebugLoggerProvider>("Microsoft", LogLevel.Information)
+                    .AddFilter<ConsoleLoggerProvider>("Microsoft", LogLevel.Trace))
+            .ConfigureServices((_, services) =>
+            services.AddAWSService<IAmazonCloudWatch>()
+            .AddTransient<CloudWatchWrapper>()
+        )
+        .Build();
 
+        _configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("settings.json") // Load settings from .json file.
+            .AddJsonFile("settings.local.json",
+                true) // Optionally, load local settings.
+            .Build();
+
+        logger = LoggerFactory.Create(builder => { builder.AddConsole(); })
+                              .CreateLogger<Program>();
+
+        _cloudWatchWrapper = host.Services.GetRequiredService<CloudWatchWrapper>();
+
+        Console.WriteLine(new string('-', 80));
+        Console.WriteLine("Amazon CloudWatch Metrics Retriever.");
+        Console.WriteLine(new string('-', 80));
+
+        // Run metric functions here
+        try
+        {
+
+        }
+        catch (Exception ex) { logger.LogError(ex, "There was a problem executing the scenario."); }
     }
 
 
