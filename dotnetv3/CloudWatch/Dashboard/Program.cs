72542452329740dbba7c64c720e5131c5aebc946
Holly DuCoing <71049594+HDuCoing@users.noreply.github.com>
2023-07-04 15:10:07 +1200

    -

--- dotnetv3/CloudWatch/Dashboard/Program.cs
+++ dotnetv3/CloudWatch/Dashboard/Program.cs
@@ -11,6 +11,10 @@ using Microsoft.Extensions.Logging.Console;
 using Microsoft.Extensions.Logging.Debug;
 using CloudWatchActions;
 using CloudWatchScenario;
+using Amazon.EC2;
+using Newtonsoft;
+using Newtonsoft.Json;
+using Dashboard;
 
 namespace Program;
 public class Program
@@ -52,71 +56,103 @@ public class Program
         Console.WriteLine("Amazon CloudWatch Metrics Retriever.");
         Console.WriteLine(new string('-', 80));
 
+
         // Run metric functions here
         try
         {   // set the namespace - there is no default and without namespace the metrics can be
             // allocated to the wrong area on accident
-            var selectedNamespace = await SelectNamespace();
+           
+            var selectedNamespace = "AWS/EC2";
+            UpdateJson(region:"ap-southeast-2",dashboard:"Metrics-DEV", accountid:"427510574968", instancetype:"t3a.medium");
+            await CreateDashboard();
+            //await CleanupResources();
             
 
         }
         catch (Exception ex) { logger.LogError(ex, "There was a problem executing the scenario."); }
     }
-
+    // Creates a dashboard in the accounts AWS Cloudwatch account
     private static async Task CreateDashboard()
-    {
+    {   // Dashboard name from settings.json and model
         Console.WriteLine("Creating Dashboard...");
         var dashboardName = _configuration["dashboardName"];
         var newDashboard = new DashboardModel();
-        //TODO - update new dashboard info here
-        // Add a new metric to the dashboard.
-        newDashboard.Widgets.Add(new Widget
-        {
-            Height = 8,
-            Width = 8,
-            Y = 8,
-            X = 0,
-            Type = "metric",
-            Properties = new Properties
-            {
-                Metrics = new List<List<object>>
-                    { //todo
-                      },
-                View = "timeSeries",
-                Region = _configuration["region"],
-                Stat = "Sum",
-                Period = 86400,
-                YAxis = new YAxis { Left = new Left { Min = 0, Max = 100 } },
-                Title = "Custom Metric Widget",
-                LiveData = true,
-                Sparkline = true,
-                Trend = true,
-                Stacked = false,
-                SetPeriodToTimeRange = false
-            }
-        });
+        // Cloudwatch statistics
+        var client = new AmazonCloudWatchClient();
 
-        var newDashboardString = JsonSerializer.Serialize(newDashboard,
-            new JsonSerializerOptions
-            { DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull });
-        var validationMessages =
-            await _cloudWatchWrapper.PutDashboard(dashboardName, newDashboardString);
+        var dimension = new Dimension
+        {
+            Name = dashboardName,
+            Value = "Virtual Machine Metric",
+            Namespace
+        };
+
+        
+        //,new JsonSerializerOptions { DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull }
+        var newDashboardString = System.Text.Json.JsonSerializer.Serialize(newDashboard);
+        await _cloudWatchWrapper.PutDashboard(dashboardName, newDashboardString);
     }
 
-
-    private static async Task<string> SelectNamespace()
+    // Optionally cleans up the dashboard by deleting it
+    private static async Task CleanupResources()
     {
         Console.WriteLine(new string('-', 80));
-        Console.WriteLine($"CloudWatch Namespace");
-        var metrics = await _cloudWatchWrapper.ListMetrics();
-        // namespace = targeted dashboard??
-        var selectedNamespace = _configuration["dashboardName"];
+        Console.WriteLine($"18. Clean up resources.");
+
+        var dashboardName = _configuration["dashboardName"];
+        if (GetYesNoResponse($"\tDelete dashboard {dashboardName}? (y/n)"))
+        {
+            Console.WriteLine($"\tDeleting dashboard.");
+            var dashboardList = new List<string> { dashboardName };
+            await _cloudWatchWrapper.DeleteDashboards(dashboardList);
+        }
+
+        var alarmName = _configuration["exampleAlarmName"];
+        if (GetYesNoResponse($"\tDelete alarm {alarmName}? (y/n)"))
+        {
+            Console.WriteLine($"\tCleaning up alarms.");
+            var alarms = new List<string> { alarmName };
+            await _cloudWatchWrapper.DeleteAlarms(alarms);
+        }
+
+        if (GetYesNoResponse($"\tDelete anomaly detector? (y/n)") && anomalyDetector != null)
+        {
+            Console.WriteLine($"\tCleaning up anomaly detector.");
+
+            await _cloudWatchWrapper.DeleteAnomalyDetector(
+                anomalyDetector);
+        }
 
         Console.WriteLine(new string('-', 80));
-
-        return selectedNamespace;
+    }
+    private static bool GetYesNoResponse(string question)
+    {
+        Console.WriteLine(question);
+        var ynResponse = Console.ReadLine();
+        var response = ynResponse != null &&
+                       ynResponse.Equals("y",
+                           StringComparison.InvariantCultureIgnoreCase);
+        return response;
     }
 
+    private static async Task ProvideMetrics()
+    {
+        var client = new AmazonEC2Client();
 
-    
+    }
+    // Convert settings.json to jsonobject to update the values with the needed information.
+    private static void UpdateJson(string region, string dashboard, string accountid, string instancetype)
+    {
+        
+        string json = File.ReadAllText("settings.json");
+        var settings = JsonConvert.DeserializeObject<Settings>(json);
+
+        settings.InstanceTypes[0].InstanceType = instancetype;
+        settings.dashboardName = dashboard;
+        settings.accountId = accountid;
+        settings.region = region;
+        string output = JsonConvert.SerializeObject(settings, Formatting.Indented);
+
+        File.WriteAllText("settings.json", output);
+    }
 }
---
+++ dotnetv3/CloudWatch/Dashboard/settings.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Dashboard;
+
+public class EbsInfo
+{
+    public string EbsOptimizedSupport { get; set; }
+    public string EncryptionSupport { get; set; }
+}
+
+public class InstanceTypes
+{
+    public string InstanceType { get; set; }
+    public bool CurrentGeneration { get; set; }
+    public bool FreeTierEligible { get; set; }
+    public List<string> SupportedUsageClasses { get; set; }
+    public List<string> SupportedRootDeviceTypes { get; set; }
+    public bool BareMetal { get; set; }
+    public string Hypervisor { get; set; }
+    public ProcessorInfo ProcessorInfo { get; set; }
+    public VCpuInfo VCpuInfo { get; set; }
+    public MemoryInfo MemoryInfo { get; set; }
+    public bool InstanceStorageSupported { get; set; }
+    public EbsInfo EbsInfo { get; set; }
+    public NetworkInfo NetworkInfo { get; set; }
+    public PlacementGroupInfo PlacementGroupInfo { get; set; }
+    public bool HibernationSupported { get; set; }
+    public bool BurstablePerformanceSupported { get; set; }
+    public bool DedicatedHostsSupported { get; set; }
+    public bool AutoRecoverySupported { get; set; }
+}
+
+public class MemoryInfo
+{
+    public int SizeInMiB { get; set; }
+}
+
+public class NetworkInfo
+{
+    public string NetworkPerformance { get; set; }
+    public int MaximumNetworkInterfaces { get; set; }
+    public int Ipv4AddressesPerInterface { get; set; }
+    public int Ipv6AddressesPerInterface { get; set; }
+    public bool Ipv6Supported { get; set; }
+    public string EnaSupport { get; set; }
+}
+
+public class PlacementGroupInfo
+{
+    public List<string> SupportedStrategies { get; set; }
+}
+
+public class ProcessorInfo
+{
+    public List<string> SupportedArchitectures { get; set; }
+    public double SustainedClockSpeedInGhz { get; set; }
+}
+
+public class Settings
+{
+    public string dashboardName { get; set; }
+    public string exampleAlarmName { get; set; }
+    public string accountId { get; set; }
+    public string region { get; set; }
+    public List<InstanceTypes> InstanceTypes { get; set; }
+}
+
+public class VCpuInfo
+{
+    public int DefaultVCpus { get; set; }
+    public int DefaultCores { get; set; }
+    public int DefaultThreadsPerCore { get; set; }
+    public List<int> ValidCores { get; set; }
+    public List<int> ValidThreadsPerCore { get; set; }
+}
+
